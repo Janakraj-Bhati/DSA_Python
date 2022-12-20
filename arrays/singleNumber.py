69588ecf9d10f325a074e6e5328c11f91488d8c1
Janakraj-Bhati <j.bhati@protonmail.com>
2022-12-20 20:18:14 +0530

    updated arrays questions

--- arrays/singleNumber.py
+++ arrays/singleNumber.py
@@ -7,13 +7,3 @@ class Solution:
         for i in nums:
             a ^= i
         return a
-    
-    
-# 2nd Solution
-# Time Limit Exceeded
-
-class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
-        for i in nums:
-            if nums.count(i) == 1:
-                return i
--- arrays/twoSum.py
+++ arrays/twoSum.py
@@ -8,14 +8,3 @@ class Solution:
             r = target - j
             if r in d: return [d[r], i]
             d[j] = i
-            
-            
-# 2nd Solution
-# Time Limit Exceeded
-
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
